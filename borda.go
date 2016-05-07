7fa8efd80badbe8fca9b9fd2af399f3e93f912d0
Ox Cart <ox.to.a.cart@gmail.com>
2016-05-07 13:33:54 -0500

    Removed references to downsampling

--- borda.go
+++ borda.go
@@ -5,23 +5,20 @@ import (
 )
 
 // Measurement represents a measurement at a point in time. It maps to a "point"
-// in InfluxDB. borda collects Measurements and saves them to the database in
-// batches. During this process, borda may downsample Measurements by grouping
-// them by their Dimensions (including IndexedDimensions) and then using the
-// most recent Gauges and Counts and aggregating all Increments.
+// in InfluxDB.
 type Measurement struct {
 	// Key is the name of the measurement (e.g. cpu_usage).
-	Key string
+	Key string `json:"key"`
 
 	// Timestamp records the time of the measurement.
-	Timestamp time.Time
+	Timestamp time.Time `json:"timestamp,omitempty"`
 
 	// IndexedDimensions capture metadata about the measurement. IndexedDimensions
 	// are best for bounded values with relatively small cardinality. They map to
 	// "tags" in InfluxDB.
 	//
 	// Example: {"host": "myhost.mydomain.com", "total_cpus": "2"}
-	IndexedDimensions map[string]string
+	IndexedDimensions map[string]string `json:"indexed_dimensions,omitempty"`
 
 	// UnindexedDimensions capture metadata about the measurement.  They map to
 	// "fields" in InfluxDB. Unlikely IndexedDimensions, it's okay to include
@@ -30,27 +27,21 @@ type Measurement struct {
 	//
 	// Example: {"requestid": "18af517b-004f-486c-9978-6cf60be7f1e9",
 	//           "ipv6": "2001:0db8:0a0b:12f0:0000:0000:0000:0001"}
-	UnindexedDimensions map[string]interface{}
+	UnindexedDimensions map[string]interface{} `json:"unindexed_dimensions,omitempty"`
 
 	// Gauges represent a snapshot of a constantly shifting measurement at a point
-	// in time. During downsampling, we keep the most recent gauge for a given set
-	// of dimensions.
+	// in time.
 	//
 	// Example: {"idle": 10.1, "system": 53.3, "user": 36.6}
-	Gauges map[string]float64
+	Gauges map[string]float64 `json:"gauges,omitempty"`
 
 	// Counts is a map of counts associated with the measurement, keyed to their
-	// names. During downsampling, we keep the most recent count for a given
-	// set of Dimensions
+	// names.
 	//
 	// Example: {"swapouts": 512343, "swapins": 64534}
-	Counts map[string]int64
+	Counts map[string]int64 `json:"counts,omitempty"`
+}
 
-	// Increments are like Counts except that they represent deltas during a time
-	// period rather than a total count at a point in time. During downsampling,
-	// increments will automatically be aggregated into the most recent point,
-	// grouped by the Dimensions.
-	//
-	// Example: {"numerrors": 5}
-	Increments map[string]int64
+// Collector collects Measurements.
+type Collector struct {
 }
