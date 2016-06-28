40ad0acf4beb1e3709b487883e2eb26e2fcc60d0
Ox Cart <ox.to.a.cart@gmail.com>
2016-06-28 13:32:15 -0500

    Lower resolution, smaller hot period

--- tdb.go
+++ tdb.go
@@ -11,8 +11,8 @@ import (
 
 // TDBSave creates a SaveFN that saves to an embedded tdb.DB
 func TDBSave(dir string) (SaveFunc, error) {
-	resolution := 5 * time.Minute
-	hotPeriod := 10 * time.Minute
+	resolution := 1 * time.Minute
+	hotPeriod := 2 * time.Minute
 	retentionPeriod := 1 * time.Hour
 
 	db := tdb.NewDB(&tdb.DBOpts{
