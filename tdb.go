96a47ace243bbf8e82c2fa25545ab9ef5eba7041
Ox Cart <ox.to.a.cart@gmail.com>
2016-06-28 17:28:04 -0500

    Tracking inserted and dropped points

--- tdb.go
+++ tdb.go
@@ -12,7 +12,7 @@ import (
 func TDBSave(dir string) (SaveFunc, error) {
 	resolution := 5 * time.Minute
 	hotPeriod := 10 * time.Minute
-	retentionPeriod := 4 * time.Hour
+	retentionPeriod := 1 * time.Hour
 
 	db := tdb.NewDB(&tdb.DBOpts{
 		Dir:       dir,
@@ -30,13 +30,13 @@ func TDBSave(dir string) (SaveFunc, error) {
 		ticker := time.NewTicker(5 * time.Second)
 		for range ticker.C {
 			stats := db.TableStats("combined")
-			log.Debugf("Hot Keys: %v     Archived Buckets: %v", humanize.Comma(stats.HotKeys), humanize.Comma(stats.ArchivedBuckets))
+			log.Debugf("Inserted Points: %v\tDropped Points: %v\tHot Keys: %v\tArchived Buckets:\t%v", humanize.Comma(stats.InsertedPoints), humanize.Comma(stats.DroppedPoints), humanize.Comma(stats.HotKeys), humanize.Comma(stats.ArchivedBuckets))
 		}
 	}()
 
 	return func(m *Measurement) error {
 		return db.Insert("combined", &tdb.Point{
-			Ts:   time.Now(),
+			Ts:   m.Ts,
 			Dims: m.Dimensions,
 			Vals: m.Values,
 		})
