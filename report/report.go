d08947ad39ec188e23420753d04cde32a6d5bfbc
Ox Cart <ox.to.a.cart@gmail.com>
2016-07-01 15:51:53 -0500

    Debugging reports

--- report/report.go
+++ report/report.go
@@ -1,6 +1,7 @@
 package report
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -65,6 +66,12 @@ func (h *Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		resp.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(resp, "%v\n", err)
+		return
+	}
+
+	if true {
+		enc := json.NewEncoder(resp)
+		enc.Encode(result)
 		return
 	}
 
