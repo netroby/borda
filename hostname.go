662f7e5913a464f1f1a76adb561e2b2dc656b5cf
Ox Cart <ox.to.a.cart@gmail.com>
2016-10-18 08:06:30 -0500

    Fixed HOSTNAME function to work with both srvip->srv and srvip->server

--- hostname.go
+++ hostname.go
@@ -5,24 +5,41 @@ import (
 	"github.com/getlantern/goexpr"
 	"github.com/hashicorp/golang-lru"
 	"gopkg.in/redis.v3"
+	"time"
 )
 
 var (
-	redisClient *redis.Client
+	redisClient   *redis.Client
+	hostnameCache *lru.Cache
 )
 
 func SetRedis(client *redis.Client) {
 	redisClient = client
+	hostnameCache, _ = lru.New(100000)
+	go warmCache(client)
+}
+
+func warmCache(redisClient *redis.Client) {
+	for {
+		names, err := redisClient.HGetAllMap("srvip->server").Result()
+		if err != nil {
+			log.Errorf("Unable to get all servers from Redis: %v", err)
+		} else {
+			hostnameCache.Purge()
+			for ip, name := range names {
+				hostnameCache.Add(ip, name)
+			}
+		}
+		time.Sleep(5 * time.Minute)
+	}
 }
 
 func BuildHostname(ip goexpr.Expr) goexpr.Expr {
-	cache, _ := lru.New(100000)
-	return &hostnameExpr{ip, cache}
+	return &hostnameExpr{ip}
 }
 
 type hostnameExpr struct {
-	ip    goexpr.Expr
-	cache *lru.Cache
+	ip goexpr.Expr
 }
 
 func (e *hostnameExpr) Eval(params goexpr.Params) interface{} {
@@ -30,20 +47,22 @@ func (e *hostnameExpr) Eval(params goexpr.Params) interface{} {
 	if ip == nil {
 		return nil
 	}
-	cached, found := e.cache.Get(ip)
+	cached, found := hostnameCache.Get(ip)
 	if found {
 		return cached
 	}
 	ipString := ip.(string)
-	name := ""
-	srv, _ := redisClient.HGet("srvip->srv", ipString).Result()
-	if srv != "" {
-		name, _ = redisClient.HGet("srv->name", srv).Result()
+	name, _ := redisClient.HGet("srvip->server", ipString).Result()
+	if name == "" {
+		srv, _ := redisClient.HGet("srvip->srv", ipString).Result()
+		if srv != "" {
+			name, _ = redisClient.HGet("srv->name", srv).Result()
+		}
 	}
 	if name == "" {
 		name = ipString
 	}
-	e.cache.Add(ip, name)
+	hostnameCache.Add(ip, name)
 	return name
 }
 
